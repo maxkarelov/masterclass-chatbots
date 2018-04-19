43d19892ee4fc9f968a14ad4c4fb2ca00b7bb201
Boris Filippov <bfilippov@griddynamics.com>
2018-04-19 17:09:32 +0400

    Add integration with DF

--- bot/botHandler.js
+++ bot/botHandler.js
@@ -5,6 +5,7 @@
  * @param {!Object} res Cloud Function контекст ответа.
  */
 const request = require('request');
+const dialogflow = require('dialogflow');
 
 // TODO: заменить <REPLACE> на свои значения из настроек VK бота группы
 const VK_GROUP_ID = '<REPLACE>';
@@ -18,31 +19,58 @@ const VK_API_VERSION = '5.74';
 const MESSAGE_NEW = 'message_new';
 const CONFIRMATION = 'confirmation';
 
-handleMessage = (mess) => {
-  // TODO: добавить вызов DiglogFlow
-  return `ответ бота: ${mess}`
+// TODO: Заменить <REPLACE> на project id своего агента в dialogflow
+const DF_PROJECT_ID = '<PROJECT_NAME>';
+
+handleMessage = (user_id, mess) => {
+  const sessionClient = new dialogflow.SessionsClient();
+  const sessionPath = sessionClient.sessionPath(DF_PROJECT_ID, "1");
+  let request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: mess,
+        languageCode: "RU",
+      }
+    }
+  }
+  sessionClient.detectIntent(request).then(responses => {
+    const response = responses[0];
+    request.get(renderUrl(user_id, response.queryResult), function(error,
+      res,
+      body) {
+      console.log('statusCode:', res && res.statusCode);
+      res.status(200).send('ok');
+    });
+  });
+}
+
+echoMessage = (user_id, mess) => {
+  request.get(renderUrl(user_id, `ответ бота: ${mess}`),
+    function(error,
+      res,
+      body) {
+      console.log('statusCode:', res && res.statusCode);
+      res.status(200).send('ok');
+    });
 }
 
 renderUrl = (user_id, message) => {
-  return `${VK_MESSAGE_API}?access_token=${VK_ACCESS_TOKEN}&user_id=${user_id}&message=${encodeURIComponent(message)}&v=${VK_API_VERSION}`;
+  return
+    `${VK_MESSAGE_API}?access_token=${VK_ACCESS_TOKEN}&user_id=${user_id}&message=${encodeURIComponent(message)}&v=${VK_API_VERSION}`;
 }
 
-sendMessage = (user_id, mess) => {
-  request.get(renderUrl(user_id, handleMessage(mess)), function (error, res, body) {
-    console.log('statusCode:', response && response.statusCode);
-  });
-}
 
 exports.botHandler = (req, res) => {
   if (req.body.type == CONFIRMATION && req.body.group_id == VK_GROUP_ID) {
     res.status(200).send(VK_CONFIRMATION_CODE);
-  } else if (req.body.type == MESSAGE_NEW && req.body.group_id == VK_GROUP_ID && req.body.secret == VK_SECRET) {
+  } else if (req.body.type == MESSAGE_NEW && req.body.group_id ==
+    VK_GROUP_ID &&
+    req.body.secret == VK_SECRET) {
     const user_id = req.body.object.user_id;
     const message = req.body.object.body;
 
-    sendMessage(user_id, message);
-    res.status(200).send('ok');
-
+    echoMessage(user_id, message);
   } else {
     res.status(400).send('invalid request');
   }
